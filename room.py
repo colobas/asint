711591803e57af2aeec76b913a968e735a9b74c4
miguelpoliveira <mpomiguel@hotmail.com>
2017-01-15 22:13:44 +0000

    room actualized

--- room.py
+++ room.py
@@ -1,6 +1,8 @@
 class Room:
 
-    def __init__ (self, id, name, capacity):
+    def __init__ (self, id, name, campus, building, capacity):
         self.id=id
         self.name=name
+        self.campus=campus
+        self.building=building
         self.capacity=capacity
